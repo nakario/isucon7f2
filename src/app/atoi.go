e9132ae818da3b72967e4c5f56948abbd9c11dc1
murata <murata@kmc.gr.jp>
2018-09-30 20:38:24 +0900

    my atoi

--- src/app/atoi.go
+++ src/app/atoi.go
@@ -4,22 +4,25 @@
 
 package main
 
+
 const fastSmalls = true // enable fast path for small integers
 const nSmalls = 100
-const smallsString = "00010203040506070809" +
-	"10111213141516171819" +
-	"20212223242526272829" +
-	"30313233343536373839" +
-	"40414243444546474849" +
-	"50515253545556575859" +
-	"60616263646566676869" +
-	"70717273747576777879" +
-	"80818283848586878889" +
-	"90919293949596979899"
+var smallsString = []byte{
+	'0','0','0','1','0','2','0','3','0','4','0','5','0','6','0','7','0','8','0','9',
+	'1','0','1','1','1','2','1','3','1','4','1','5','1','6','1','7','1','8','1','9',
+	'2','0','2','1','2','2','2','3','2','4','2','5','2','6','2','7','2','8','2','9',
+	'3','0','3','1','3','2','3','3','3','4','3','5','3','6','3','7','3','8','3','9',
+	'4','0','4','1','4','2','4','3','4','4','4','5','4','6','4','7','4','8','4','9',
+	'5','0','5','1','5','2','5','3','5','4','5','5','5','6','5','7','5','8','5','9',
+	'6','0','6','1','6','2','6','3','6','4','6','5','6','6','6','7','6','8','6','9',
+	'7','0','7','1','7','2','7','3','7','4','7','5','7','6','7','7','7','8','7','9',
+	'8','0','8','1','8','2','8','3','8','4','8','5','8','6','8','7','8','8','8','9',
+	'9','0','9','1','9','2','9','3','9','4','9','5','9','6','9','7','9','8','9','9',
+}
 const host32bit = ^uint(0)>>32 == 0
-const digits = "0123456789abcdefghijklmnopqrstuvwxyz"
+var digits = []byte{'0','1','2','3','4','5','6','7','8','9'}
 // small returns the string for an i with 0 <= i < nSmalls.
-func small(i int) string { // WARN:
+func small(i int) []byte { // WARN:
 	if i < 10 {
 		return digits[i : i+1]
 	}
@@ -28,13 +31,12 @@ func small(i int) string { // WARN:
 
 func FormatInt(i int64) []byte {
 	if 0 <= i && i < nSmalls {
-		return []byte(small(int(i)))
+		return small(int(i))
 	}
-	return formatBits(nil, uint64(i), i < 0, false)
+	return formatBits(nil, uint64(i), i < 0)
 }
 
-func formatBits(dst []byte, u uint64, neg, append_ bool) []byte {
-
+func formatBits(dst []byte, u uint64, neg bool) []byte {
 	var a [64 + 1]byte
 	i := len(a)
 
@@ -95,10 +97,6 @@ func formatBits(dst []byte, u uint64, neg, append_ bool) []byte {
 	if neg {
 		i--
 		a[i] = '-'
-	}
-
-	if append_ {
-		return append(dst, a[i:]...)
 	}
 	return a[i:]
 }
