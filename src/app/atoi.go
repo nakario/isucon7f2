af693b7ed98c17108f5e626b2398b0ea7e37f0ca
murata <murata@kmc.gr.jp>
2018-09-30 23:41:34 +0900

    ffjson

--- src/app/atoi.go
+++ src/app/atoi.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-package app
+package main
 
 const fastSmalls = true // enable fast path for small integers
 const nSmalls = 100
